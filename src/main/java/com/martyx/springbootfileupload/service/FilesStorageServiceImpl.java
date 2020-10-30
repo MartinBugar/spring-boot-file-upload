7b4998124a94327ad78ac38e3ee8e9af2fc1f00d
prometheus9911 <martin1.bugar@gmail.com>
2020-10-30 21:58:15 +0100

    FilesStorageServiceImpl

--- src/main/java/com/martyx/springbootfileupload/service/FilesStorageServiceImpl.java
+++ src/main/java/com/martyx/springbootfileupload/service/FilesStorageServiceImpl.java
@@ -1,4 +1,48 @@
 package com.martyx.springbootfileupload.service;
 
-public class FilesStorageServiceImpl {
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FilesStorageServiceImpl implements FilesStorageService {
+
+    private final Path root = Paths.get("uploads");
+
+    @Override
+    public void init() {
+        try {
+            Files.createDirectories(root);
+        } catch (IOException ex){
+            throw new RuntimeException("Could not initialize folder for upload");
+        }
+    }
+
+    @Override
+    public void save(MultipartFile file) {
+        try {
+            Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
+        }catch (IOException ex){
+            throw new RuntimeException("Could not store the file " + ex.getMessage());
+        }
+    }
+
+    @Override
+    public Resource load(String filename) {
+        return null;
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public Stream<Path> loadAll() {
+        return null;
+    }
 }
