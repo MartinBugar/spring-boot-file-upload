031e817c48ce889542b569d707aa33d9269c2a72
Martin <martin1.bugar@gmail.com>
2020-10-31 15:25:18 +0100

    Controller

--- src/main/java/com/martyx/springbootfileupload/controller/FilesController.java
+++ src/main/java/com/martyx/springbootfileupload/controller/FilesController.java
@@ -4,12 +4,12 @@ import com.martyx.springbootfileupload.message.ResponseMessage;
 import com.martyx.springbootfileupload.models.FileInfo;
 import com.martyx.springbootfileupload.service.FilesStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
@@ -24,28 +24,38 @@ public class FilesController {
     FilesStorageService filesStorageService;
 
     @PostMapping("/upload")
-    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file")MultipartFile file){
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
         String message = "";
         try {
             filesStorageService.save(file);
 
             message = "Uploaded the file succesfully : " + file.getOriginalFilename();
             return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
-        } catch (Exception ex){
+        } catch (Exception ex) {
             message = "Could not upload the file: " + file.getOriginalFilename() + " !";
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
         }
     }
 
-    public ResponseEntity<List<FileInfo>> getListFiles(){
+    public ResponseEntity<List<FileInfo>> getListFiles() {
         List<FileInfo> fileInfos = filesStorageService.loadAll().map(path -> {
             String filename = path.getFileName().toString();
             String url = MvcUriComponentsBuilder
                     .fromMethodName(FilesController.class, "getFile", path.getFileName().toString())
                     .build()
                     .toString();
-            return new FileInfo(filename,url);
+            return new FileInfo(filename, url);
         }).collect(Collectors.toList());
         return ResponseEntity.status(HttpStatus.OK).body(fileInfos);
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        org.springframework.core.io.Resource file = filesStorageService.load(filename);
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
     }
 }
